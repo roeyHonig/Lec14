2a7965c1ce203571429af0f728d76d2184bb1354
roeyHonig <roeyhonig94@gmail.com>
2018-06-07 16:21:54 +0300

    with delete and indexpath.row not index path.item !!!!!!!!!

--- Lec14/StudentsTableViewController.swift
+++ Lec14/StudentsTableViewController.swift
@@ -58,32 +58,38 @@ class StudentsTableViewController: UITableViewController {
 
         // Configure the cell...
         
-        cell.firstNameLabel.text = students[indexPath.item].firstname
-        cell.lastNameLabel.text = students[indexPath.item].lastname
-        cell.emailLabel.text = students[indexPath.item].email
+        cell.firstNameLabel.text = students[indexPath.row].firstname
+        cell.lastNameLabel.text = students[indexPath.row].lastname
+        cell.emailLabel.text = students[indexPath.row].email
         return cell
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            // HomeWork
+            
+            // delete a student from the index *Memory
+            students.remove(at: indexPath.row)
+            
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
