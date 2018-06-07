e1f2c92dfc208e0dc1e8f31e1624fe15a5b5876d
roeyHonig <roeyhonig94@gmail.com>
2018-06-07 15:36:01 +0300

    some GUI finally

---
+++ Lec14/StudentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentTableViewCell.swift
+//  Lec14
+//
+//  Created by hackeru on 24 Sivan 5778.
+//  Copyright © 5778 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
